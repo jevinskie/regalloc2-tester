5348474e3f2b3c339367cf6a01468b2ad7e01567
demilade <sonugademilade8703@gmail.com>
2024-03-18 16:42:30 +0100

    now successfully runs regalloc for the original algorithm on inputs

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,62 @@
+use regalloc2::{
+    Allocation, Block, Edit, Inst, InstOrEdit, InstRange, MachineEnv, Operand, PReg, PRegSet,
+    RegClass, RegallocOptions, VReg,
+};
 use std::sync::atomic::AtomicUsize;
-
-use regalloc2::{Block, VReg, Inst, Operand, OperandConstraint, OperandKind, OperandPos, RegClass, InstRange, PRegSet};
 
 fn main() {
     let (input, no_of_regs) = get_input();
     let builder = IrBuilder::new();
     let ir = builder.build(input.lines().collect());
     println!("{:?}", ir);
+    let output = regalloc2::run(
+        &ir,
+        &MachineEnv {
+            preferred_regs_by_class: [
+                (0..no_of_regs)
+                    .into_iter()
+                    .map(|num| PReg::new(num, RegClass::Int))
+                    .collect(),
+                vec![],
+                vec![],
+            ],
+            non_preferred_regs_by_class: [vec![], vec![], vec![]],
+            scratch_by_class: [None, None, None],
+            fixed_stack_slots: vec![],
+        },
+        &RegallocOptions {
+            verbose_log: true,
+            validate_ssa: true,
+        },
+    )
+    .unwrap();
+    println!("{:?}\n", output);
+
+    let mut finalcode = String::new();
+    for blocknum in 0..ir.blocks.len() {
+        finalcode.push_str(&format!("block {blocknum}\n"));
+        for out in output.block_insts_and_edits(&ir, Block::new(blocknum)) {
+            match out {
+                InstOrEdit::Inst(inst) => {
+                    let instn = ir.find_instn(inst).unwrap();
+                    let allocs = output.inst_allocs(inst);
+                    finalcode.push_str(&format!("\t{}", instn.rewrite(allocs)));
+                }
+                InstOrEdit::Edit(edit) => match edit {
+                    Edit::Move { from, to } => {
+                        finalcode.push_str(&format!(
+                            "\tmove {} to {}",
+                            from.to_string(),
+                            to.to_string()
+                        ));
+                    }
+                },
+            }
+            finalcode.push('\n');
+        }
+        finalcode.push('\n');
+    }
+    println!("{}", finalcode);
 }
 
 static MAX_VREG_NO: AtomicUsize = AtomicUsize::new(0);
@@ -31,9 +81,7 @@ impl Ir {
 
 impl regalloc2::Function for Ir {
     fn num_insts(&self) -> usize {
-        self.blocks.iter()
-            .map(|block| block.instns.len())
-            .sum()
+        self.blocks.iter().map(|block| block.instns.len()).sum()
     }
 
     fn entry_block(&self) -> Block {
@@ -64,13 +112,15 @@ impl regalloc2::Function for Ir {
     }
 
     fn is_ret(&self, insn: Inst) -> bool {
-        let instn = self.find_instn(insn)
+        let instn = self
+            .find_instn(insn)
             .expect(&format!("Failed to find instruction with index {:?}", insn));
         instn.is_ret()
     }
 
     fn is_branch(&self, insn: Inst) -> bool {
-        let instn = self.find_instn(insn)
+        let instn = self
+            .find_instn(insn)
             .expect(&format!("Failed to find instruction with index {:?}", insn));
         instn.is_branch()
     }
@@ -87,7 +137,8 @@ impl regalloc2::Function for Ir {
     }
 
     fn inst_operands(&self, insn: Inst) -> &[Operand] {
-        let instn = self.find_instn(insn)
+        let instn = self
+            .find_instn(insn)
             .expect(&format!("Failed to find instruction with index {:?}", insn));
         instn.operands()
     }
@@ -142,9 +193,14 @@ impl IrBuilder {
                     if words.len() < 2 {
                         panic!("Expected the block definition to have a block number");
                     }
-                    self.blockindex = words[1].parse::<usize>().expect("Block numbers should be valid `usize`s");
+                    self.blockindex = words[1]
+                        .parse::<usize>()
+                        .expect("Block numbers should be valid `usize`s");
                     for word in &words[2..] {
-                        assert!(word.starts_with("v"), "Block params must have valid virtual register names");
+                        assert!(
+                            word.starts_with("v"),
+                            "Block params must have valid virtual register names"
+                        );
                         assert!(word.len() > 1, "Virtual registers should have a number");
                         let regnum = word[1..].parse::<usize>().unwrap();
                         self.blockparams.push(VReg::new(regnum, RegClass::Int));
@@ -153,35 +209,41 @@ impl IrBuilder {
                 "if" => {
                     let instn = Instn::cond_branch(words, self.next_instn_index);
                     self.succs.push(instn.to());
+                    self.succs.push(instn.elseto());
                     self.instns.push(instn);
+                    println!("{:?} {:?}", self.next_instn_index, line);
+                    self.next_instn_index += 1;
                 }
                 "goto" => {
                     let instn = Instn::uncond_branch(words, self.next_instn_index);
                     self.succs.push(instn.to());
                     self.instns.push(instn);
+                    println!("{:?} {:?}", self.next_instn_index, line);
+                    self.next_instn_index += 1;
                 }
                 "output" => {
                     assert_eq!(words.len(), 2);
                     let reg = parse_vreg(words[1]);
                     self.instns.push(Instn {
                         index: Inst::new(self.next_instn_index),
-                        instntype: InstnType::Output([Operand::new(
-                            reg,
-                            OperandConstraint::Any,
-                            OperandKind::Use,
-                            OperandPos::Late
-                        )])
+                        instntype: InstnType::Output([Operand::reg_use(reg)]),
                     });
+                    println!("{:?} {:?}", self.next_instn_index, line);
+                    self.next_instn_index += 1;
                 }
                 "return" => {
                     assert_eq!(words.len(), 1);
                     self.instns.push(Instn {
                         index: Inst::new(self.next_instn_index),
                         instntype: InstnType::Return,
-                    })
+                    });
+                    println!("{:?} {:?}", self.next_instn_index, line);
+                    self.next_instn_index += 1;
                 }
                 v => {
-                    if v.starts_with("v") {
+                    if v.starts_with("#") {
+                        continue;
+                    } else if v.starts_with("v") {
                         let parts: Vec<&str> = line.split("=").collect();
                         assert_eq!(parts.len(), 2);
                         let outreg = parse_vreg(parts[0].trim());
@@ -189,20 +251,29 @@ impl IrBuilder {
                         assert!(!input.is_empty());
                         let cmd = input[0];
                         if cmd == "input" {
-                            self.instns.push(Instn::input(outreg, self.next_instn_index));
+                            self.instns
+                                .push(Instn::input(outreg, self.next_instn_index));
                         } else {
-                            self.instns.push(Instn::new(cmd, outreg, &input[1..], self.next_instn_index));
+                            self.instns.push(Instn::new(
+                                cmd,
+                                outreg,
+                                &input[1..],
+                                self.next_instn_index,
+                            ));
                         }
+                        println!("{:?} {:?}", self.next_instn_index, line);
+                        self.next_instn_index += 1;
                     } else {
                         panic!("Unrecognized word: {}", v);
                     }
                 }
             };
-            self.next_instn_index += 1;
         }
         self.end_bb();
         self.compute_preds();
-        Ir { blocks: self.blocks }
+        Ir {
+            blocks: self.blocks,
+        }
     }
 
     fn end_bb(&mut self) {
@@ -231,22 +302,29 @@ impl IrBuilder {
 #[derive(Clone, Debug)]
 struct Instn {
     index: Inst,
-    instntype: InstnType
+    instntype: InstnType,
 }
 
 impl Instn {
     fn to(&self) -> Block {
-        match self.instntype {
-            InstnType::CondBranch { to, .. } => to,
-            InstnType::Branch { to, .. } => to,
-            _ => panic!("Attempting to find a `to` on a non-branch instruction")
+        match &self.instntype {
+            InstnType::CondBranch { to, .. } => to.0,
+            InstnType::Branch { to, .. } => to.clone(),
+            _ => panic!("Attempting to find a `to` on a non-branch instruction"),
+        }
+    }
+
+    fn elseto(&self) -> Block {
+        match &self.instntype {
+            InstnType::CondBranch { elseto, .. } => elseto.0,
+            _ => panic!("Attempting to find an `elseto` on a non-cond-branch instruction"),
         }
     }
 
     fn is_ret(&self) -> bool {
         match &self.instntype {
             InstnType::Return => true,
-            _ => false
+            _ => false,
         }
     }
 
@@ -254,25 +332,33 @@ impl Instn {
         match &self.instntype {
             InstnType::CondBranch { .. } => true,
             InstnType::Branch { .. } => true,
-            _ => false
+            _ => false,
         }
     }
 
     fn blockparams_to(&self, succidx: usize) -> &[VReg] {
         match &self.instntype {
-            InstnType::CondBranch { to, branchargs, .. } => {
-                if to.index() == succidx {
-                    &branchargs
+            InstnType::CondBranch { to, elseto, .. } => {
+                if succidx == 0 {
+                    &to.1
+                } else if succidx == 1 {
+                    &elseto.1
                 } else {
-                    eprintln!("The branch doesnt go to succidx");
+                    eprintln!(
+                        "There is no {succidx} successor for instruction {:?}",
+                        self.index
+                    );
                     &[]
                 }
             }
-            InstnType::Branch { to, branchargs, .. } => {
-                if to.index() == succidx {
-                    eprintln!("The branch doesnt go to succidx");
+            InstnType::Branch { branchargs, .. } => {
+                if succidx == 0 {
                     &branchargs
                 } else {
+                    eprintln!(
+                        "There is no {succidx} successor for instruction {:?}",
+                        self.index
+                    );
                     &[]
                 }
             }
@@ -285,20 +371,12 @@ impl Instn {
 
     fn operands(&self) -> &[Operand] {
         match &self.instntype {
-            InstnType::Normal { operands, .. } => {
-                &operands
-            }
-            InstnType::CondBranch { args, .. } => {
-                args.as_slice()
-            }
-            InstnType::Input(op) => {
-                op.as_slice()
-            }
-            InstnType::Output(op) => {
-                op.as_slice()
-            }
+            InstnType::Normal { operands, .. } => &operands,
+            InstnType::CondBranch { args, .. } => args.as_slice(),
+            InstnType::Input(op) => op.as_slice(),
+            InstnType::Output(op) => op.as_slice(),
             _ => {
-                println!("No operands");
+                //println!("No operands for instruction {:?}", self.index);
                 &[]
             }
         }
@@ -309,7 +387,7 @@ impl Instn {
 enum InstnOperand {
     /// An index into a vector of operands
     Operand(usize),
-    Constant(String)
+    Constant(String),
 }
 
 #[derive(Clone, Debug)]
@@ -323,8 +401,8 @@ enum InstnType {
     CondBranch {
         cmp: String,
         args: [Operand; 2],
-        to: Block,
-        branchargs: Vec<VReg>,
+        to: (Block, Vec<VReg>),
+        elseto: (Block, Vec<VReg>),
     },
     Branch {
         to: Block,
@@ -337,66 +415,58 @@ enum InstnType {
 
 impl Instn {
     fn new(cmd: &str, outreg: VReg, args: &[&str], index: usize) -> Self {
-        let mut operands = vec![Operand::new(
-            outreg.clone(),
-            OperandConstraint::Any,
-            OperandKind::Def,
-            OperandPos::Late
-        )];
+        let mut operands = vec![Operand::reg_def(outreg.clone())];
         let mut parsedargs = vec![];
         for arg in args {
             if arg.starts_with("v") {
-                operands.push(Operand::new(
-                    parse_vreg(arg),
-                    OperandConstraint::Any,
-                    OperandKind::Use,
-                    OperandPos::Early
-                ));
+                operands.push(Operand::reg_use(parse_vreg(arg)));
                 parsedargs.push(InstnOperand::Operand(operands.len() - 1));
             } else {
                 parsedargs.push(InstnOperand::Constant(arg.to_string()));
             }
         }
-
         Self {
             index: Inst::new(index),
             instntype: InstnType::Normal {
                 cmd: cmd.to_string(),
                 args: parsedargs,
                 operands,
-            }
+            },
         }
     }
 
     fn cond_branch(words: Vec<&str>, index: usize) -> Self {
-        assert!(words.len() >= 6);
-        let firstarg = Operand::new(
-            parse_vreg(words[1]),
-            OperandConstraint::Any,
-            OperandKind::Use,
-            OperandPos::Early,
-        );
+        assert!(words.len() >= 9);
+        let firstarg = Operand::reg_use(parse_vreg(words[1]));
         let cmp = words[2];
-        let secondarg = Operand::new(
-            parse_vreg(words[3]),
-            OperandConstraint::Any,
-            OperandKind::Use,
-            OperandPos::Early
-        );
+        let secondarg = Operand::reg_use(parse_vreg(words[3]));
         assert_eq!(words[4], "goto");
         let to = words[5].parse::<usize>().unwrap();
-        let mut branchargs = vec![];
-        for reg in &words[6..] {
-            branchargs.push(parse_vreg(reg));
+        let mut toargs = vec![];
+        let mut i = 6;
+        while i < words.len() && words[i] != "else" {
+            toargs.push(parse_vreg(words[i]));
+            i += 1;
+        }
+        assert!(i < words.len(), "There should be an else branch");
+        // After else, there should at least be a "goto [branch num]"
+        assert!(words[i + 1..].len() >= 2);
+        assert_eq!(words[i + 1], "goto");
+        let elseto = words[i + 2].parse::<usize>().unwrap();
+        let mut elsetoargs = vec![];
+        let mut j = i + 2 + 1;
+        while j < words.len() {
+            elsetoargs.push(parse_vreg(words[j]));
+            j += 1;
         }
         Self {
             index: Inst::new(index),
             instntype: InstnType::CondBranch {
                 cmp: cmp.to_string(),
                 args: [firstarg, secondarg],
-                to: Block::new(to),
-                branchargs
-            }
+                to: (Block::new(to), toargs),
+                elseto: (Block::new(elseto), elsetoargs),
+            },
         }
     }
 
@@ -404,26 +474,91 @@ impl Instn {
         assert!(words.len() >= 2);
         let to = Block::new(words[1].parse::<usize>().unwrap());
         let mut branchargs = vec![];
-        if words.len() >= 2 {
+        if words.len() > 2 {
             for reg in &words[2..] {
                 branchargs.push(parse_vreg(reg));
             }
         }
         Self {
             index: Inst::new(index),
-            instntype: InstnType::Branch { to, branchargs }
+            instntype: InstnType::Branch { to, branchargs },
         }
     }
 
     fn input(out: VReg, index: usize) -> Self {
         Self {
             index: Inst::new(index),
-            instntype: InstnType::Input([Operand::new(
-                out,
-                OperandConstraint::Any,
-                OperandKind::Def,
-                OperandPos::Late,
-            )])
+            instntype: InstnType::Input([Operand::reg_def(out)]),
+        }
+    }
+
+    fn rewrite(&self, allocs: &[Allocation]) -> String {
+        match &self.instntype {
+            InstnType::Normal {
+                cmd,
+                args,
+                operands,
+            } => {
+                assert_eq!(allocs.len(), operands.len());
+                let mut code = format!(
+                    "{} = {} ",
+                    if allocs[0].is_reg() {
+                        allocs[0].as_reg().unwrap().to_string()
+                    } else {
+                        allocs[0].as_stack().unwrap().to_string()
+                    },
+                    cmd
+                );
+                for operand in args.iter() {
+                    match operand {
+                        InstnOperand::Operand(op) => {
+                            let op = *op;
+                            if allocs[op].is_reg() {
+                                code.push_str(&allocs[op].as_reg().unwrap().to_string());
+                            } else {
+                                code.push_str(&allocs[op].as_stack().unwrap().to_string());
+                            }
+                            code.push(' ');
+                        }
+                        InstnOperand::Constant(c) => {
+                            code.push_str(c);
+                        }
+                    }
+                }
+                code
+            }
+            InstnType::CondBranch {
+                cmp,
+                args,
+                to,
+                elseto,
+            } => {
+                format!(
+                    "if {} {} {} goto {:?} else goto {:?}",
+                    allocs[0], cmp, allocs[1], to.0, elseto.0
+                )
+            }
+            InstnType::Branch { to, branchargs } => {
+                format!("goto {:?} ", to)
+            }
+            InstnType::Output(ops) => {
+                if allocs[0].is_reg() {
+                    format!("output {}", allocs[0].as_reg().unwrap().to_string())
+                } else {
+                    format!("output {}", allocs[0].as_stack().unwrap().to_string())
+                }
+            }
+            InstnType::Input(ops) => {
+                let op = ops[0];
+                if allocs[0].is_reg() {
+                    format!("{} = input", allocs[0].as_reg().unwrap().to_string())
+                } else {
+                    format!("{} = input", allocs[0].as_stack().unwrap().to_string())
+                }
+            }
+            InstnType::Return => {
+                format!("return")
+            }
         }
     }
 }
@@ -447,7 +582,10 @@ fn parse_vreg(reg: &str) -> VReg {
 
 fn update_max_vreg(regnum: usize) {
     use std::sync::atomic::Ordering;
-    MAX_VREG_NO.store(MAX_VREG_NO.load(Ordering::Relaxed).max(regnum), Ordering::Relaxed);
+    MAX_VREG_NO.store(
+        MAX_VREG_NO.load(Ordering::Relaxed).max(regnum),
+        Ordering::Relaxed,
+    );
 }
 
 fn get_max_vreg() -> usize {
@@ -455,7 +593,7 @@ fn get_max_vreg() -> usize {
     MAX_VREG_NO.load(Ordering::Relaxed)
 }
 
-fn get_input() -> (String, i32) {
+fn get_input() -> (String, usize) {
     let args: Vec<String> = std::env::args().collect();
     if args.len() != 3 {
         fail("USAGE: prog [input filepath] [num of physical registers]");
